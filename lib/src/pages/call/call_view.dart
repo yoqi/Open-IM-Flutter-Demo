b55c583a37aa8fe9e726376fe7465a5a4285fe5d
boyrobot <44001185+boyrobot@users.noreply.github.com>
2023-05-30 15:10:27 +0000

    0

--- lib/src/pages/call/call_view.dart
+++ lib/src/pages/call/call_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+/// Description: call page
+/// Time       : 05/30/2023 Tuesday
+/// Author     : liuyuqi.gov@msn.cn
 class CallPage extends StatelessWidget {
   const CallPage({Key? key}) : super(key: key);
 
